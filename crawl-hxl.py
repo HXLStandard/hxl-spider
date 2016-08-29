760456c4aeb1bd4d1bdb83985bbd057989daa583
David Megginson <david.megginson@megginson.com>
2016-08-29 15:19:49 -0400

    Major change: print disaggregated data in JSON rather than simple aggregated stats.

--- crawl-hxl.py
+++ crawl-hxl.py
@@ -11,45 +11,51 @@ import ckanapi, hxl, json, sys, time
 # read configuration values from config.py
 import config
 
-delay = 5
+DELAY = 1
 """Time delay in seconds between files, to give source APIs a rest."""
 
 ckan = ckanapi.RemoteCKAN(config.CONFIG['ckanurl'], apikey=config.CONFIG['apikey'], user_agent=config.CONFIG.get('user_agent', None))
 """The CKAN API object"""
 
-def process_dataset(dataset, count_maps):
-    """Do something with a dataset tagged hxl"""
 
-    def increment(count_maps, type, key):
-        if not count_maps.get(type):
-            count_maps[type] = dict()
-        if count_maps[type].get(key):
-            count_maps[type][key] += 1
-        else:
-            count_maps[type][key] = 1
-            
+def process_dataset(dataset):
+    """Do something with a dataset tagged hxl"""
+    record = {
+        'type': 'dataset',
+        'name': dataset['name'],
+        'title': dataset['title'],
+        'owner_org': dataset['owner_org'],
+        'source': dataset['dataset_source'],
+        'resources': []
+    }
     for resource in dataset['resources']:
         try:
+            resource_info = {
+                'type': 'resource',
+                'name': resource['name'],
+                'url': resource['url'],
+                'columns': []
+            }
             columns = hxl.data(resource['url']).columns
+            resource_info['columns'].append([{
+                'tag': column.tag,
+                'display_tag': column.display_tag,
+                'attributes': list(column.attributes)
+            } for column in columns])
+            record['resources'].append(resource_info)
         except:
-            print("  Skipped {}".format(resource['name']), file=sys.stderr)
-            return
-        for column in columns:
-            increment(count_maps, 'tags', column.tag)
-            increment(count_maps, 'display_tags', column.display_tag)
-            for attribute in column.attributes:
-                increment(count_maps, 'attributes', attribute)
-        if count_maps.get('total'):
-            count_maps['total'] +=1
-        else:
-            count_maps['total'] = 1
-        print("  Indexed {}".format(resource['name']), file=sys.stderr)
-        print("  Waiting {} seconds ...".format(delay), file=sys.stderr)
-        time.sleep(delay)
+            print("  Skipped {} (not valid HXL)".format(resource['name']), file=sys.stderr)
+            return None
+    if record['resources']:
+        return record
+    else:
+        return False
+        
 
 def find_hxl_datasets(start, rows):
     """Return a page of HXL datasets."""
     return ckan.action.package_search(start=start, rows=rows, fq="tags:hxl")
+
 
 def is_hxl(url):
     """Try to parse as a HXL dataset."""
@@ -59,31 +65,33 @@ def is_hxl(url):
     except:
         return False
     
+    
 def crawl_datasets():
     """Crawl through all datasets tagged 'hxl'"""
     index = 0
-    start = 0
-    rows = 25
-    result = find_hxl_datasets(start, rows)
-    total = result['count']
-    count_maps = dict()
+    result_start_pos = 0
+    result_page_size = 25
+    result = find_hxl_datasets(result_start_pos, result_page_size)
+    result_total_count = result['count']
 
-    print("Found {} datasets.".format(total), file=sys.stderr)
+    print("Found {} datasets.".format(result_total_count), file=sys.stderr)
 
-    while start < total:
+    while result_start_pos < result_total_count:
         for dataset in result['results']:
             index += 1
             print("Dataset {}: {}".format(index, dataset['name']), file=sys.stderr)
-            process_dataset(dataset, count_maps)
-        start += rows
-        result = find_hxl_datasets(start, rows)
-
-    return count_maps
+            record = process_dataset(dataset)
+            if record:
+                print(json.dumps(record, indent=4) + ',')
+        result_start_pos += result_page_size
+        result = find_hxl_datasets(result_start_pos, result_page_size)
+        time.sleep(DELAY)
 
 #
 # Main loop
 #
-count_maps = crawl_datasets()
-print(json.dumps(count_maps, indent=4))
+print("[");
+crawl_datasets()
+print("]")
 
 # end
